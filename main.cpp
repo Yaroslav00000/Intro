d364647334e2f22cab1364a5c77ea027c521851a
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 22:13:18 +0300

    Task completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include<iostream>
+#include <Windows.h>
 using namespace std;
+enum Color
+{
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    LightBrown = 14,
 
+};
 int main() {
-
-	return 0;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    cout << "To be,";
+    SetConsoleTextAttribute(hConsole, 12);
+    cout << " or not ";
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "to be: that is the question:" << endl;
+    cout << "Whether";
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "'tis nobler in the mind to suffer" << endl;
+    cout << "The slings and arrows of outrageous fortune," << endl;
+    cout << "Or to take arms against a sea of troubles," << endl;
+    cout << "And by opposing end them? " << endl;
+    cout << "William Shakespeare" << endl;
+    SetConsoleTextAttribute(hConsole, 7);
+    return 0;
 }
