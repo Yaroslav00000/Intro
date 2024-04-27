7e452d6f9a335b06012bead571d958682fa7379f
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 19:53:53 +0300

    task completed

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "\tПоет і природа" << endl;
+    cout << "\nПройшла гроза — і знову літня проза:" << endl;
+    cout << "Парує степ, і оживає ліс." << endl;
+    cout << "І горобці, неначе з-за куліс," << endl;
+    cout << "\nУпали табуном на просо." << endl;
+    cout << "І знов спадають трафаретні роси" << endl;
+    cout << "Із сінокосів буйних кіс," << endl;
+    cout << "І знову в небі невідомий біс\n" << endl;
+    cout << "\tВасиль Симоненко" << endl;
+    return 0;
+}
