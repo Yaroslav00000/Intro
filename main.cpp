27c405a5d4660000ecc56fcdd45430f1db8a506b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-04-27 22:08:57 +0300

    Task completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include <Windows.h>
 using namespace std;
+enum Color
+{
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    LightBrown = 14,
 
+};
 int main() {
-
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "\"Hello, Word!\"" << endl;
+    SetConsoleTextAttribute(hConsole, 7);
+    cout << "/""Bjarne Stroustrup""/" << endl;
 	return 0;
 }
